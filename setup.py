f9a017aeecf1da00f89069f35b381f0ac985ebb8
Georg Brandl <g.brandl@fz-juelich.de>
2022-11-25 14:20:05 +0100

    bump to 1.3
    Change-Id: If82463f0528a2b61ca2723c146e2a291d1eb78bc

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, Extension
 
 setup(name='nicos-pyctl',
-      version = '1.1',
+      version='1.3',
       description='NICOS pyctl c module',
       author='Georg Brandl',
       author_email='georg.brandl@frm2.tum.de',
-      url='http://tacodb.frm2.tum.de/pub/python/',
+      url='https://forge.frm2.tum.de/simple/nicos-pyctl/',
       packages=['nicospyctl'],
       ext_modules=[Extension('nicospyctl.pyctl', ['pyctl.c'])],
      )
